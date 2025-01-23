14c5881ba931718702e2beb5e2eecf4b79b8e0ba
externalPointerVariable <vicky986847@gmail.com>
2025-01-23 16:10:48 +0000

    chnages 1402

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,27 +1,19 @@
 from fastapi import FastAPI
-from users import fastapi_users, auth_backend
-from models.user import UserCreate, UserUpdate, UserRead
-from db import database, async_engine, base
+from pydantic import BaseModel
 
 app = FastAPI()
 
-app.include_router(fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"])
-app.include_router(fastapi_users.get_register_router(UserRead, UserCreate), prefix="/auth", tags=["auth"])
-app.include_router(fastapi_users.get_users_router(UserRead, UserUpdate), prefix="/users", tags=["users"])
-
-@app.on_event("startup")
-async def on_startup():
-    await database.connect()
-    async with async_engine.begin() as conn:
-        await conn.run_sync(base.metadata.create_all)
-
-@app.on_event("shutdown")
-async def on_shutdown():
-    await database.disconnect()
+class user(BaseModel):
+    name: str
+    email: str
+    password: str
+    async def create_user(user: user):
+        return user
 
 @app.get("/")
-async def read_root():
-    return {"message": "Welcome to the Personalized Fan Highlights API"}
+async def root():
+    return {"message": "Welcome ti mlb-personalized-ai-api"}
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+@app.post("/user")
+async def create_user(user: user):
+    return user.create_user(user)
