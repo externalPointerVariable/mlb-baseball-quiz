525b62d30f816b2a98bd0dfc8b7554525555e356
externalPointerVariable <vicky986847@gmail.com>
2025-01-29 17:12:18 +0000

    created the databse modals

--- backend/mlb/core/models.py
+++ backend/mlb/core/models.py
@@ -2,8 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-class user(models.Model):
-    pass
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50)
+    user_performance  = models.IntegerField(default=0)
+    favourite_team = models.CharField(max_length=50)
+    favourite_player = models.CharField(max_length=50)
 
-class quiz(models.Model):
-    pass
+class Quiz(models.Model):
+    quiz_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, to_field='user_id')
+    topic_name = models.CharField(max_length=50)
+    difficulty_level = models.IntegerField()
+    correct_answer = models.IntegerField()
+    total_questions = models.IntegerField()
+
