e7d0c424229f77ba6ed603fb39f3e2ca67bf39a0
externalPointerVariable <vicky986847@gmail.com>
2025-01-31 17:45:01 +0000

    Setting up the vertex AI ✅

--- backend/mlb/core/quizGen.py
+++ backend/mlb/core/quizGen.py
@@ -1,6 +1,15 @@
-from dotenv import load_dotenv
+import vertexai
 import os
-
+from dotenv import load_dotenv
+from vertexai.preview.generative_models import GenerativeModel
 load_dotenv()
 
-gemini_api_key = os.getenv("GEMINI_API_KEY")
+project_id = os.getenv('PROJECT_ID')
+location = 'us-central1'
+
+vertexai.init(project=project_id, location=location)
+
+model = GenerativeModel('gemini-pro')
+response = model.generate_content('Say hi')
+
+print(response.text)
