58dad15de8acc7b645d1518dc1ae71eaa218d119
externalPointerVariable <vicky986847@gmail.com>
2025-02-04 13:36:43 +0000

    FInal changes before deployment part 2

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
-set -o errexit
+#!/bin/bash
 
+# Navigate to the backend directory
+cd backend
+
+# Install the required Python packages
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Navigate to the mlb Django project directory
+cd mlb
+
+# Apply database migrations
 python manage.py migrate
-if [[ $CREATE_SUPERUSER ]];
-then
+
+# Create a superuser if the CREATE_SUPERUSER environment variable is set
+if [[ $CREATE_SUPERUSER ]]; then
   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
 fi
